fff2242487626631a3985f9673f4e084844e1aaf
Cecilia Biagiola <biagiolacecilia@gmail.com>
2022-11-03 15:33:14 -0300

    Todo Search and Todo List updated

---
+++ todo-list/src/TodoSearch/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import react from "react";
+import './TodoSearch.css';
+
+function TodoSearch({ searchValue, setSearchValue }){
+    
+    const onSearchValueChange = (event) => {
+        
+        setSearchValue(event.target.value);
+    };
+    
+    return(
+        <input 
+            className="TodoSearch" 
+            placeholder="Buscar..." 
+            value={searchValue}
+            onChange={onSearchValueChange}
+            />
+            
+    )
+    ;
+}
+export {TodoSearch};
